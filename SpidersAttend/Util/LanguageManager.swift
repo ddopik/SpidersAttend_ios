7ae9641b576113f7e4ce68a24ce6200dd70b0f67
abdalla maged <ddopik.01@gmail.com>
2019-07-28 23:05:02 +0200

    changing App icons

---
+++ SpidersAttend/Util/LanguageManager.swift
@@ -0,0 +1,9 @@
+//
+//  LanguageManager.swift
+//  SpidersAttend
+//
+//  Created by ddopik on 7/20/19.
+//  Copyright © 2019 Brandeda. All rights reserved.
+//
+
+import Foundation
---
+++ SpidersAttend/base/BaseUiTabController.swift
@@ -0,0 +1,13 @@
+//
+//  BaseUiTabController.swift
+//  SpidersAttend
+//
+//  Created by ddopik on 7/21/19.
+//  Copyright © 2019 Brandeda. All rights reserved.
+//
+
+import UIKit
+
+class BaseUiTabController: UIUserNotificationAction {
+
+}
