e6e87d86887fb262695efe8dfe05837ed5fed1ee
abdalla maged <ddopik.01@gmail.com>
2019-08-21 15:29:47 +0200

    Edit
    convert App constant and icons to Arab jet Res

--- SpidersAttend/Util/Constants.swift
+++ SpidersAttend/Util/Constants.swift
@@ -11,7 +11,7 @@ struct ApiConstant {
      static let ENTER="1"
     static let OUT="2"
     static let ENDED="3"
-     static let QR_SCANNER_CONSTANT="https://nfc.spiderholidays.co/en/"
+     static let QR_SCANNER_CONSTANT="https://hr-arabjet.spiderholidays.co/en/"
 
     
   
